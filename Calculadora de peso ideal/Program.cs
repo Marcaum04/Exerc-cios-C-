c664ae70fadc3f79e11b8967efa2ae71043c4caa
Marcos <mvinicius.oliveira04@gmail.com>
2021-05-01 21:22:44 -0300

    Primeiro exercício

---
+++ Calculadora de peso ideal/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Calculadora_de_peso_ideal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char escolha;
+            double h, pesoIdeal;
+
+            Console.WriteLine("Calculadora de peso ideal iniciada\n");
+            
+            Console.Write("Digite seu sexo (M/F): ");
+            escolha = char.Parse(Console.ReadLine());
+
+            Console.Write("Digite sua altura: ");
+            h = double.Parse(Console.ReadLine());
+
+            if(escolha == 'm' || escolha == 'M'){
+                pesoIdeal = (72.7 * h) - 58;
+                Console.WriteLine($@"Seu peso ideal é {pesoIdeal.ToString("N2")}
+Programa encerrado.");
+
+            } else if(escolha == 'f' || escolha == 'F'){
+                pesoIdeal = (62.1 * h) - 44.7;
+                Console.WriteLine($@"Seu peso ideal é {pesoIdeal.ToString("N2")}
+Programa encerrado.");
+
+            } else {
+                Console.WriteLine($@"{escolha} não é um sexo
+Programa encerrado.");
+            }
+
+        }
+    }
+}
