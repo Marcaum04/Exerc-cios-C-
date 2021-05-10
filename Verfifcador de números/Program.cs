f23de2ef113c9c9954c0b1c2641ca9c0bfb65467
Marcos <mvinicius.oliveira04@gmail.com>
2021-05-09 21:47:09 -0300

    Verificador de números

---
+++ Verfifcador de números/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Verfifcador_de_números
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numeroUm, numeroDois;
+
+            Console.WriteLine("Verificador de números iniciado\n");
+
+            Console.Write("Digite o primeiro número: ");
+            numeroUm = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o segundo número: ");
+            numeroDois = double.Parse(Console.ReadLine());
+
+            if(numeroUm == numeroDois){
+                double soma;
+                soma = numeroUm + numeroDois;
+                Console.WriteLine("\nNumeros iguais somando...");
+                Console.WriteLine($"Resultado igual {soma}");
+            }else{
+                double subtracao;
+                subtracao = numeroUm - numeroDois;
+                Console.WriteLine("\nNumeros diferentes subtraindo...");
+                Console.WriteLine($"Resultado igual = {subtracao}");
+            }
+
+            Console.WriteLine("\nPrograma finalizado");
+        }
+    }
+}
