032ace6a8bb35516f7bc6f37ed60f6c5cd56cefa
Marcos <mvinicius.oliveira04@gmail.com>
2021-05-16 23:35:02 -0300

    Identificador de triângulos

---
+++ Identificador de triângulos/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Identificador_de_triângulos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double ladoUm, ladoDois, ladoTres;
+
+            Console.Write(@"
+_________________________________________________
+||   Bem-Vindo ao identificador de triângulos  ||
+||_____________________________________________||
+||     Digite qual o tamanho dos lados do      ||
+||      triângulo que deseja identifica        ||
+|| Primeiro lado: ");
+            ladoUm = double.Parse(Console.ReadLine());
+            Console.Write("|| Segundo lado: ");
+            ladoDois = double.Parse(Console.ReadLine());
+            Console.Write("|| Terceito lado: ");
+            ladoTres = double.Parse(Console.ReadLine());
+
+            if (ladoUm == ladoDois && ladoDois == ladoTres && ladoTres == ladoUm)
+            {
+                Console.Write(@"
+__________________________________________________________________
+||   Seu triângulo é Equilátero – possui os três lados iguais   ||
+||______________________________________________________________||
+ ");
+            }
+            else if (ladoUm == ladoDois || ladoDois == ladoTres || ladoTres == ladoUm)
+            {
+                Console.Write(@"
+__________________________________________________________________
+||     Seu triângulo é Isósceles – possui dois lados iguais     ||
+||______________________________________________________________||
+ ");
+            }else{
+                Console.Write(@"
+__________________________________________________________________
+||  Seu triângulo é Escaleno – possui os três lados diferentes  ||
+||______________________________________________________________||
+ ");
+            }
+        }
+    }
+}
